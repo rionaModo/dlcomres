e3489f559ff56b85afda190f21c96ce531c7f071
lingxiaohua <lingxiaohua@danlu.com>
2017-08-18 16:26:44 +0800

    功能新增

--- Application/Home/Controller/LearnController.class.php
+++ Application/Home/Controller/LearnController.class.php
@@ -2,12 +2,12 @@
 namespace Home\Controller;
 use Think\Controller;
 class LearnController extends Controller {
-    public function learn_list(){
+    public function learn_list(){ // 页面数据
         $learn_list= D('learn_list')->select();
         $this->assign('learn_list', $learn_list);
         $this->display();
     }
-    public function create(){
+    public function create(){ //创建查看页面
         $from=I('get.from');
         $detail[]=array();
         if(!empty($from)&&$from=='edit'){
@@ -18,14 +18,14 @@ class LearnController extends Controller {
         }
         $this->display();
     }
-    function learn_insert(){
+    function learn_insert(){ //新增数据
         $learn_res= D('learn_list');
         $data=array(id=>guid(),'theme'=>I('post.theme'),'content'=>I('post.content'),type=>I('post.type'));
          $learn_res->add($data);
         header("Location:/Learn/learn_list.html");
     }
 
-    function learn_edit(){
+    function learn_edit(){ //编辑数据
         $id=I('post.id');
         $learn_res= D('learn_list');
         $data=array('theme'=>I('post.theme'),'content'=>I('post.content'),type=>I('post.type'));
